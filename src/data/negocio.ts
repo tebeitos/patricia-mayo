f1398b1f96211420fb336e485f7f4e877032dd18
tebeitos <tebeitos@users.noreply.github.com>
2026-09-07 23:29:21 +0200

    Actualizar texto descripcion servicio 01 en negocio.ts

--- src/data/negocio.ts
+++ src/data/negocio.ts
@@ -11,7 +11,7 @@ export const servicios = [
   {
     numero: "01",
     titulo: "Shootings, marcas y pasarela",
-    descripcion: "He trabajado en sesiones fotográficas para marcas como Siroko, en videoclips y pases de moda. El maquillaje que la cámara, el foco y el fotógrafo exigen es completamente distinto al de todos los días."
+    descripcion: "He trabajado en pases de moda, videoclips y en sesiones fotográficas para marcas como Siroko. El maquillaje que la cámara, el foco y el fotógrafo exigen es completamente distinto al de todos los días."
   },
   {
     numero: "02",
